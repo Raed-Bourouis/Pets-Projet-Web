19ee3c60169216babc7abfb46c69686a453c0bae
Raed-Bourouis <144831482+Raed-Bourouis@users.noreply.github.com>
2024-03-24 16:58:02 +0100

    finished l reveal button ken islem ma zedtch fiih 7aja

--- sign-in.js
+++ sign-in.js
@@ -1,12 +1,10 @@
   const passwordInput = document.getElementById('password');
   const toggleButton = document.querySelector('#revealpswrd');
 
-  function showpassword() {
+  toggleButton.addEventListener('click', function() {
     if (passwordInput.type === "password") {
       passwordInput.type = "text";
     } else {
       passwordInput.type = "password";
     }
-  }
-
-  toggleButton.addEventListener('click', showpassword() );
+  });
