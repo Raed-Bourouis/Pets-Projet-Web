7623f35e48c54417570a85e5be9e933dbd9afad4
klaoza <155117429+klaoza@users.noreply.github.com>
2024-03-26 19:56:50 +0100

    comm

--- sect7.js
+++ sect7.js
@@ -1,45 +1,50 @@
 // Get all input elements within the form
 const inputs = document.querySelectorAll('.input-control input');
-const selected=document.querySelector(".city");
+const selected = document.querySelector("#state");
 
 // Form submission event listener
 document.forms.appform.addEventListener('submit', e => {
     e.preventDefault();
     // Validate inputs
-    if (validateInputs(inputs)) {
+    if (validateInputs(inputs) && validateSelect()) {
         e.target.submit();
     }
 });
 
-// Function to show error animation
-const showErrorAnimation = parentElement => {
-    parentElement.classList.add('error');
-    parentElement.classList.add('error-animation'); 
-    setTimeout(() => {
-        parentElement.classList.remove('error-animation'); 
-    }, 500);
-};
-
-
-// Function to validate inputs 
+// Function to validate inputs
 const validateInputs = elements => {
     let isValid = true;
     elements.forEach(element => {
         const parentElement = element.parentElement;
 
         if (element.value.trim() === '') {
-            showError(element, parentElement);
-            showErrorAnimation(parentElement); 
+            showError(parentElement);
+            showErrorAnimation(parentElement);
             isValid = false;
         } else {
-            showSuccess(element, parentElement);
+            showSuccess(parentElement);
         }
     });
     return isValid;
 };
 
+// Function to validate the select dropdown
+const validateSelect = () => {
+    let isValid = true;
+    let selectIndex = document.getElementById("state").selectedIndex
+
+    if (selectIndex === 0) {
+        showError(selected);
+        showErrorAnimation(selected);
+        isValid = false;
+    } else {
+        showSuccess(selected); 
+    }
+    return isValid;
+};
+
 // Function to show error message
-const showError = (element, parentElement) => {
+const showError = parentElement => {
     const errorMessage = parentElement.querySelector('.error');
     errorMessage.innerText = '* This field is required';
     parentElement.classList.add('error');
@@ -47,9 +52,17 @@ const showError = (element, parentElement) => {
 };
 
 // Function to show success
-const showSuccess = (element, parentElement) => {
+const showSuccess = parentElement => {
     const errorMessage = parentElement.querySelector('.error');
     errorMessage.textContent = '';
     parentElement.classList.add('success');
     parentElement.classList.remove('error');
+};
+
+// Function to show error animation
+const showErrorAnimation = parentElement => {
+    parentElement.classList.add('error-animation');
+    setTimeout(() => {
+        parentElement.classList.remove('error-animation');
+    }, 500);
 };
