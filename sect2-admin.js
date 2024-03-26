39fdc9d452d111a17187a1f4c149da1146606ba2
rayenne11 <66142092+rayenne11@users.noreply.github.com>
2024-03-26 02:52:16 +0100

    c bn

---
+++ sect2-admin.js
@@ -0,0 +1,29 @@
+
+function ToDelete(button){
+    var parentDiv=button.parentNode
+    for(let i=0;i<3;i++){
+        parentDiv=parentDiv.parentNode
+    }
+    parentDiv.parentNode.removeChild(parentDiv);
+}
+
+function changerImage(link,elem){
+    elem.style='background-color: black'
+    var parentCircles=elem.parentNode
+    var intervalle=parentCircles.querySelectorAll('div')
+    intervalle.forEach(element => {
+        if(element !=elem){
+            element.style='background-color: #CCC;'
+        }
+        
+    });
+    var parentIm=elem.parentNode.parentNode
+    var imElem=parentIm.querySelector('img')
+    imElem.src=link
+    imElem.style='width: 450px; height: 350px ; border-radius: 23px'
+
+
+}
+
+
+
